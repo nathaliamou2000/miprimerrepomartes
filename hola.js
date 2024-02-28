93c2ee095be6f85b249adf55f2bc20dc92253c6a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-02-27 20:34:31 -0500

    nt3 feat se programan condiciones del login

---
+++ hola.js
@@ -0,0 +1,11 @@
+//variable global
+
+let nombre="james rodriguez" 
+
+//variable local
+if(5>2){
+    let nombre="lucho"
+}
+console.log("hola"+nombre)//imprimo en el global, tener vaariables globales 
+
+//let por var, si uso la primera usa a james y si uso var usa lucho
---
+++ main.py
@@ -0,0 +1,9 @@
+usuarioBD=" hidrotech123"
+passwordDB="admin123*"
+print('Software Tech Hidroituango')
+print('**************************')
+userName=input('Digita tu usuario: ') 
+userPassword = input ("Digita tu contraseña:  ")#a que es iguai a un input
+##tengo dos caminos if y else
+
+
